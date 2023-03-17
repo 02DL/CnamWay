e2f9aba72e8ed6a6f4f91e29985dc44b15917137
Feuille2912 <73909078+Feuille2912@users.noreply.github.com>
2023-03-17 10:02:46 +0000

    test

--- scripts/itineraire.js
+++ scripts/itineraire.js
@@ -1,15 +1,15 @@
-fetch("https://api-ratp.pierre-grimaud.fr/v4/traffic/metros/1")
-// récupére la réponse du serveur
-.then(response => {
-    // lit le contenu JSON dans le corps de la réponse:
-    return response.json()
-})
-// récupére le contenu JSON:
-.then(json => {
-    console.log(json);
-    // ici on pourrait mettre à jour l'interface avec les données de l'API
-})
-// capture les éventuelles erreur:
-.catch(error => {
-    console.error(`Oops, went wrong: ${error.code} => ${error.message}`);
-})
+fetch('https://api-ratp.pierre-grimaud.fr/v4/traffic/metros')
+  .then(response => response.json())
+  .then(data => {
+    const metros = data.result.metros;
+    const list = document.createElement('ul');
+
+    metros.forEach(metro => {
+      const item = document.createElement('li');
+      item.innerText = `Ligne ${metro.line} : ${metro.title}`;
+      list.appendChild(item);
+    });
+
+    document.body.appendChild(list);
+  })
+  .catch(error => console.error(error));
