0b8080ca444818eea824f4212e3c36a69a8ceb55
Feuille2912 <73909078+Feuille2912@users.noreply.github.com>
2023-03-17 09:29:47 +0000

    test

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+var mymap = L.map('mapid').setView([48.8566, 2.3522], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+	maxZoom: 18,
+}).addTo(mymap);
+
+var marker = L.marker([0, 0]).addTo(mymap);
+
+function onLocationFound(e) {
+	var radius = e.accuracy / 2;
+	marker.setLatLng(e.latlng).bindPopup("Vous êtes dans un rayon de " + radius + " mètres de ce point").openPopup();
+	L.circle(e.latlng, radius).addTo(mymap);
+}
+
+function onLocationError(e) {
+	alert(e.message);
+}
+
+mymap.on('locationfound', onLocationFound);
+mymap.on('locationerror', onLocationError);
+
+mymap.locate({setView: true, maxZoom: 16});
