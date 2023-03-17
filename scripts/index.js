e31590960c5ee19fa814c975797fa29277777353
Feuille2912 <73909078+Feuille2912@users.noreply.github.com>
2023-03-17 10:03:38 +0000

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,15 @@
-var mymap = L.map('mapid').setView([48.8566, 2.3522], 13);
+fetch('https://api-ratp.pierre-grimaud.fr/v4/traffic/metros')
+  .then(response => response.json())
+  .then(data => {
+    const metros = data.result.metros;
+    const list = document.createElement('ul');
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-	maxZoom: 18,
-}).addTo(mymap);
+    metros.forEach(metro => {
+      const item = document.createElement('li');
+      item.innerText = `Ligne ${metro.line} : ${metro.title}`;
+      list.appendChild(item);
+    });
 
-var marker = L.marker([0, 0]).addTo(mymap);
-
-function onLocationFound(e) {
-	var radius = e.accuracy / 2;
-	marker.setLatLng(e.latlng).bindPopup("Vous êtes dans un rayon de " + radius + " mètres de ce point").openPopup();
-	L.circle(e.latlng, radius).addTo(mymap);
-}
-
-function onLocationError(e) {
-	alert(e.message);
-}
-
-mymap.on('locationfound', onLocationFound);
-mymap.on('locationerror', onLocationError);
-
-mymap.locate({setView: true, maxZoom: 16});
+    document.body.appendChild(list);
+  })
+  .catch(error => console.error(error));
