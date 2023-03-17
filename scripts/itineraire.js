ae8f5b8b621356f1e26d6a0cca4bb3b4ab04aa05
Feuille2912 <73909078+Feuille2912@users.noreply.github.com>
2023-03-17 09:54:05 +0000

    tet

---
+++ scripts/itineraire.js
@@ -0,0 +1,15 @@
+fetch("https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/4/chatelet/R")
+// récupére la réponse du serveur
+.then(response => {
+    // lit le contenu JSON dans le corps de la réponse:
+    return response.json()
+})
+// récupére le contenu JSON:
+.then(json => {
+    console.log(json);
+    // ici on pourrait mettre à jour l'interface avec les données de l'API
+})
+// capture les éventuelles erreur:
+.catch(error => {
+    console.error(`Oops, went wrong: ${error.code} => ${error.message}`);
+})
