c21c0707e1c402786d5efad30a028f9efc5b2b05
02DL <95748958+02DL@users.noreply.github.com>
2023-03-21 20:44:54 +0000

    alerte perturbation

---
+++ scripts/alerte.js
@@ -0,0 +1,25 @@
+
+//alert(document.cookie);
+
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=0'
+}
+
+$(document).ready(function() {
+    let cookieValue = getCookie("alerte");
+    // Afficher la valeur du cookie
+    $('#alerte').html(cookieValue);
+  });
+
+
+// récupérer la valeur d'un cookie par son nom
+function getCookie(name) {
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i].trim();
+      if (cookie.indexOf(name + '=') === 0) {
+        return cookie.substring(name.length + 1, cookie.length);
+      }
+    }
+    return '';
+  }
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+//etat du traffic metro
 fetch('https://api-ratp.pierre-grimaud.fr/v4/traffic/metros')
   .then(response => response.json())
   .then(data => {
@@ -8,17 +9,58 @@ fetch('https://api-ratp.pierre-grimaud.fr/v4/traffic/metros')
 
     metros.forEach(metro => {
       const item = document.createElement('li');
-      item.innerText = `Ligne ${metro.line} : ${metro.title}`;
+      item.innerText = `Ligne ${metro.line} : ${metro.message}`;
       list.appendChild(item);
 
-
-      if ( `${metro.title}`  != "Trafic normal") { isNormal = false;};
+      if ( `${metro.title}`  != "Trafic normal") { 
+        isNormal = false;
+        msg = `${metro.line} `;
+        addToAlerte(msg)};
     });
 
     if(isNormal) {
-      document.body.append("Le trafic est normal dans l'ensemble des lignes. ")
+      document.body.append("Le trafic est normal dans l'ensemble des lignes METRO. ")
     } else { document.body.appendChild(list);
     };
 
   })
   .catch(error => console.error(error));
+
+  //etat du traffic rer
+  fetch('https://api-ratp.pierre-grimaud.fr/v4/traffic/rers')
+  .then(response => response.json())
+  .then(data => {
+    const rers = data.result.rers;
+    const list = document.createElement('ul');
+
+    var isNormal = new Boolean(true);
+
+    rers.forEach(rer => {
+      const item = document.createElement('li');
+      item.innerText = `Ligne ${rer.line} : ${rer.title}`;
+      list.appendChild(item);
+
+      if ( `${rers.line}`  != "Trafic normal") { 
+        isNormal = false;
+        msg = `${rer.line} `;
+        addToAlerte(msg)};
+    });
+
+    if(isNormal) {
+      document.body.append("Le trafic est normal dans l'ensemble des lignes RER. ")
+    } else { document.body.appendChild(list);
+    };
+
+  })
+  .catch(error => console.error(error));
+
+var alerteMsg ="Perturbations sur les lignes: ";
+var msg = "";
+
+function addToAlerte(msg){
+  alerteMsg += msg;
+  document.cookie = "alerte=" + alerteMsg;
+}
+
+ 
+
