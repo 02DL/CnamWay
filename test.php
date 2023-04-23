926c2e75d5a79e59ea66ed3c5e2dd069403f77cb
Feuille2912 <73909078+Feuille2912@users.noreply.github.com>
2023-04-23 12:30:18 +0200

    test

--- test.php
+++ test.php
@@ -1,7 +1,8 @@
+
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-<?php
 
 // informations de connexion à la base de données
 $serveur = "localhost";
