8e353b8607c7ed242ff338935819591d440ad6ff
Feuille2912 <73909078+Feuille2912@users.noreply.github.com>
2023-04-23 12:38:35 +0200

    test

---
+++ testphp.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Exemple</title>
+		<meta charset="UTF-8">
+    </head>
+    <body>
+        <?php 
+          include("test2.php"); 
+        ?>
+    </body>
+</html>
