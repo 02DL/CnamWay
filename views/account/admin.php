ec37b6a7c003cd376756e0990da7d0ffe8acd0a5
02DL <dngls02@gmail.com>
2023-05-09 16:18:17 +0200

    test

--- views/account/admin.php
+++ views/account/admin.php
@@ -1,1 +1,48 @@
-<h1>Administrer</h1>
+      <nav>
+        <ul>
+          <li><a href="index.php?c=account/admin">Déclaration d'incident</a></li>
+          <li><a href="index.php?c=account/userList">Gestion des comptes</a></li>
+        </ul>
+      </nav>
+     
+
+<h1>Déclarer un incident</h1>
+
+<a href="index.php?c=process_signOut">Sign Out</a>
+
+<form action="index.php?c=process_incident" method="POST">
+  <div class="form-group">
+    <label for="typeT">Quel moyen de transport est concerné ?</label>
+    <select name="typeT" id="typeT">
+    <option value="">--Choisissez le moyen de transport--</option>
+    <option value="RER">RER</option>
+    <option value="METRO">METRO</option>
+    </select>
+
+  </div>
+  <div class="form-group">
+    <label for="ligneT">Quelle ligne ?</label>
+    <select name="ligneT" id="ligneT">
+    <option value="">--Choisissez la ligne concernée--</option>
+    <option value="dog">Dog</option>
+    <option value="cat">Cat</option>
+    </select>
+
+  </div>
+  <div class="form-group">
+    <label for="typeI">Quel type d'incident ?</label>
+    <select name="typeI" id="typeI">
+    <option value="">--Choisissez le type d'incident--</option>
+    <option value="Perturbation">Perturbation</option>
+    <option value="Incident">Incident</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="messageI">Message</label>
+    <textarea rows ="5" id="messageI" name="messageI" required></textarea>
+  </div>
+  <button type="submit">Envoyer</button>
+
+</form>
+
+document.cookie = "alerte=" + alerteMsg;
