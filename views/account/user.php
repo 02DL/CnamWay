e6365d33cc2b5661d010de6a4617235d370154b7
02DL <dngls02@gmail.com>
2023-06-06 12:15:32 +0200

    admin

--- views/account/user.php
+++ views/account/user.php
@@ -5,5 +5,24 @@ echo $_SESSION['username'];
 
 ?>
 
+<h1> Mes favoris </h1>
+
+<section class="container-loisir container text-center" id="favorisList">
+
+<?php 
+foreach ($data['data'] as $result){ 
+?>
+   
+<li> <?php echo $result; ?> </li>
+
+<?php 
+} 
+?>
+
+</section>
+
+
+
+
 <a href="index.php?c=process_signOut">Sign Out</a>
 
