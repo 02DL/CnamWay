1e1ce2d9718bf23bfbe369c1c7689e827ce32c99
02DL <dngls02@gmail.com>
2023-06-08 16:43:48 +0200

    vérifie que n'est pas deja présent dans la base

--- controllers/process_favoris.php
+++ controllers/process_favoris.php
@@ -1,7 +1,14 @@
 <?php 
-//sauvegarde la destination dans les favoris
-$stmt = $pdo->prepare('INSERT INTO favoris(FId, FDest) VALUES(null, ?)');
+$stmt = $pdo->prepare('SELECT count(*) as nb FROM favoris WHERE FDest = ?');
 $stmt->execute([$_COOKIE['destinationActuelle']]);
+$r = $stmt->fetch();
+
+//pas dans la base
+if ($r['nb'] != 1) {
+    //sauvegarde la destination dans les favoris
+    $stmt = $pdo->prepare('INSERT INTO favoris(FId, FDest) VALUES(null, ?)');
+    $stmt->execute([$_COOKIE['destinationActuelle']]);
+}
 
 //récupère id du favoris
 $stmt = $pdo->prepare('SELECT FId FROM favoris WHERE FDest =?');
@@ -13,9 +20,15 @@ $stmt = $pdo->prepare('SELECT UId FROM utilisateur WHERE UMail =?');
 $stmt->execute([$_SESSION['username']]);
 $uid = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
-//join le favoris à l'utilisateur
-$stmt = $pdo->prepare('INSERT INTO avoir(FId, UId) VALUES(?,?)');
+$stmt = $pdo->prepare('SELECT count(*) as nb FROM avoir WHERE FId = ? AND UId = ?');
 $stmt->execute([$fid[0],$uid[0]]);
+$r = $stmt->fetch();
+
+if ($r['nb'] != 1) {
+    //join le favoris à l'utilisateur
+    $stmt = $pdo->prepare('INSERT INTO avoir(FId, UId) VALUES(?,?)');
+    $stmt->execute([$fid[0],$uid[0]]);
+}
 
 redirect('itineraire');
 ?>
